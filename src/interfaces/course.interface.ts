7e5e09a6a6bae4ea52eedcb30e26689c2e0f90b2
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2024-06-30 17:41:03 +0600

    style: format code

--- src/interfaces/course.interface.ts
+++ src/interfaces/course.interface.ts
@@ -1,20 +1,20 @@
 import mongoose from 'mongoose';
 
 interface ICourse {
-    title: string;
-    instructor: string;
-    categoryId: mongoose.Schema.Types.ObjectId;
-    price: number;
-    tags: { name: string; isDeleted: boolean }[]; //array of objects
-    startDate: string;
-    endDate: string;
-    language: string;
-    provider: string;
-    //an object field
-    details: {
-        level: string;
-        description: string;
-    };
+  title: string;
+  instructor: string;
+  categoryId: mongoose.Schema.Types.ObjectId;
+  price: number;
+  tags: { name: string; isDeleted: boolean }[]; //array of objects
+  startDate: string;
+  endDate: string;
+  language: string;
+  provider: string;
+  //an object field
+  details: {
+    level: string;
+    description: string;
+  };
 }
 /**Another field is durationInWeeks:number,
  * (this field will generated from server by counting the start and end date)
--- src/models/course.model.ts
+++ src/models/course.model.ts
@@ -1,38 +1,38 @@
-import { Schema, model } from "mongoose";
-import ICourse from "../interfaces/course.interface";
+import { Schema, model } from 'mongoose';
+import ICourse from '../interfaces/course.interface';
 
 const courseSchema = new Schema<ICourse>({
-    title: {
-        type: String
-    },
-    instructor: {
-        type: String
-    },
-    categoryId: {
-        type: Schema.Types.ObjectId
-    },
-    price: {
-        type: Number
-    },
-    tags: {
-        type: [{ name: String, isDeleted: Boolean }]
-    },
-    startDate: {
-        type: String
-    },
-    endDate: {
-        type: String
-    },
-    language: {
-        type: String
-    },
-    provider: {
-        type: String
-    },
-    details: {
-        type: { level: String, description: String }
-    },
+  title: {
+    type: String,
+  },
+  instructor: {
+    type: String,
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+  },
+  price: {
+    type: Number,
+  },
+  tags: {
+    type: [{ name: String, isDeleted: Boolean }],
+  },
+  startDate: {
+    type: String,
+  },
+  endDate: {
+    type: String,
+  },
+  language: {
+    type: String,
+  },
+  provider: {
+    type: String,
+  },
+  details: {
+    type: { level: String, description: String },
+  },
 });
 
-const CourseModel=model<ICourse>("Course",courseSchema)
+const CourseModel = model<ICourse>('Course', courseSchema);
 export default CourseModel;
